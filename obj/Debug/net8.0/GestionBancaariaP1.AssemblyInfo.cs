8bf1bea28c2cd9eb7f301777822f4bfe92008929
Rafael Gomez <rafaelogomezr@gmail.com>
2024-01-30 15:11:39 -0400

    Clausuras guardianas en Eliminar Prductos

--- obj/Debug/net8.0/GestionBancaariaP1.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestionBancaariaP1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionBancaariaP1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38fce9a6d2a38b392e21bf83e00bc5c54cce7a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f3692fb9dc6cb4a2b971b636abfd4502046bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionBancaariaP1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionBancaariaP1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
